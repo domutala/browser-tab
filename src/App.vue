de86f5084eccf1cba267d26d073cdef2e3486059
Mamadou Talla DIA <ibntalla@gmail.com>
2023-12-05 16:16:31 +0000

    init project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <div>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi tenetur voluptatibus iusto,
+    error omnis in repellendus tempore aliquid ipsum. Enim quibusdam tempora explicabo tempore
+    repellat quae dignissimos at itaque eaque.
+  </div>
+</template>
